67161842fc36b10f285edfb6d0d75f0b3cbc7538
romamaxime <roma.maxime1994@gmail.com>
2017-03-09 11:51:34 +0100

    ajout de la fonction division

--- js/app.js
+++ js/app.js
@@ -9,3 +9,11 @@ var bar = function (a, b) {
 var multiple = function (a, b){
 	console.log (a * b);
 }
+
+var division = function(a, b){
+	if(b > 0) {
+		console.log(a/b);
+	} else {
+		console.log("Can't divide by 0");
+	}	
+}
