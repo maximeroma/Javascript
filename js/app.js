0b3afd7b9c4c8872f4708dd8fc02ea4216f3399f
romamaxime <roma.maxime1994@gmail.com>
2017-03-09 11:47:23 +0100

    résolution des soucis de "merge"

--- js/app.js
+++ js/app.js
@@ -4,11 +4,8 @@ var foo = function(a, b){
 
 var bar = function (a, b) {
 	console.log(a - b);	
-<<<<<<< HEAD
 }
 
 var multiple = function (a, b){
 	console.log (a * b);
-=======
->>>>>>> 6da093842c22d8870edc3fd5002924cce0016f4a
 }
